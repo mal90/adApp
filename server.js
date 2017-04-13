34aa33a8ad7b548a2ce74005b1d2e3d0c7bb6c61
mal90 <madkonline@gmail.com>
2017-04-13 20:24:06 +0530

    Connect to gitlab

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient
 const bodyParser= require('body-parser')
 const app = express();
+var CONFIG = require('./public/config.json');
+var db;
 
+var userName = CONFIG.uname;
+var passWord = CONFIG.pword;
+var dbName = CONFIG.dbname;
+
+var URL='mongodb://'+userName+':'+passWord+'@ds161190.mlab.com:61190/'+dbName;
+console.log(URL);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+MongoClient.connect(URL, (err, database) => {
+  if (err) 
+    return console.log(err);
+  db = database;
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  })
 })
+
 
 // app.get('/', (req, res) => {
 //   console.log("directory " + __dirname);
@@ -15,6 +30,6 @@ app.listen(3000, function() {
 // })
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   res.end();
 })
