9fc9ffdea4411a1385569a5d12e8d8cfe3f859eb
mal90 <madkonline@gmail.com>
2017-04-14 16:55:59 +0530

    Change logic for calculation , refactor code

--- public/js/app.js
+++ public/js/app.js
@@ -1,166 +1,177 @@
-var app = angular.module("adApp", []); 
+var app = angular.module("adApp", []);
 
-app.controller("dashboardCtrl", function($scope) {
+app.controller("dashboardCtrl", function ($scope) {
     $scope.goToAdProduct = goToAdProduct;
     $scope.goToAdCheckout = goToAdCheckout;
 
-    function goToAdProduct(){
+    function goToAdProduct() {
         window.location = '../products.html';
     }
 
-    function goToAdCheckout(){
+    function goToAdCheckout() {
         window.location = '../checkout.html';
     }
 });
 
-app.controller("checkoutCtrl", function($scope,$http) {
-    $scope.customers = ['Default','UNILEVER','APPLE','NIKE','FORD'];
+app.controller("checkoutCtrl", function ($scope, $http) {
+    $scope.customers = ['Default', 'UNILEVER', 'APPLE', 'NIKE', 'FORD'];
     $scope.adTypes = [];
     $scope.addItem = addItem;
+    $scope.changeCustomer = changeCustomer;
     $scope.totalAddedItems = [];
+    var customerSelected;
     getAllProducts();
+    $scope.totalCheckoutValue = 0;
 
-    function getAllProducts(){
+    function getAllProducts() {
         $http.get('/products').
-        success(function(data) {
-            $scope.products = data ;
-            getAllAdTypes(data);
-        }).error(function(data) {
-            console.error("error pccured");
-        })
+            success(function (data) {
+                $scope.products = data;
+                getAllAdTypes(data);
+            }).error(function (data) {
+                console.error("error pccured");
+            })
     }
 
-    function getAllAdTypes(products){
-        products.forEach(function(product) {
+    function getAllAdTypes(products) {
+        products.forEach(function (product) {
             $scope.adTypes.push(product.name);
         });
     }
 
-    function addItem(){
-        calculateTotal();
+    function changeCustomer() {
+        if (customerSelected != undefined && customerSelected != $scope.customerName) {
+            var confirmChange = confirm("You will lose your current customer info ! Click OK to proceed");
+            if (confirmChange) {
+                $scope.totalAddedItems = [];
+            } else {
+                $scope.customerName = customerSelected;
+            }
+        }
     }
-    
-    function calculateTotal(){
-        var addedItem = {};
-        if(isNaN($scope.numberOfItems)){
+
+    function addItem() {
+        var overwritten;
+
+        if (isNaN($scope.numberOfItems)) {
             alert("Count should be a valid number");
             return false;
         }
 
-        var itemExists = false ;
-        $scope.totalAddedItems.forEach(function(addedItem) {
-            if($scope.selectedProduct != undefined && $scope.selectedProduct!= null && 
-                addedItem.name === $scope.selectedProduct.name){
-                addedItem.numberOfItems =  parseInt(addedItem.numberOfItems) + parseInt($scope.numberOfItems) ;
-                addItem.discountApplied = false ;
-                addedItem.totalPrice = discountLogic(addedItem) ;   
-                itemExists = true;
+        overwritten = false;
+        $scope.totalAddedItems.forEach(function (addedItem, index) {
+            if ($scope.selectedProduct != undefined && $scope.selectedProduct != null &&
+                addedItem.name === $scope.selectedProduct.name) {
+                var confirmOverWright = confirm("Do you want to overwright the current entry under the same Ad type ? Click OK to proceed");
+                if (confirmOverWright) {
+                    $scope.totalAddedItems.splice(index, 1);
+                }
             }
         });
 
-        if(!itemExists){
-            addedItem.name = $scope.selectedProduct.name;
-            addedItem.id = $scope.selectedProduct.id;
-            addedItem.price = $scope.selectedProduct.price;
-            addedItem.numberOfItems = $scope.numberOfItems;
-            addItem.discountApplied = false ;
-            addedItem.totalPrice = discountLogic(addedItem);
-            $scope.totalAddedItems.push(addedItem);
-        }
+        addToCheckoutList();
+    }
 
-        function discountLogic(addedItem){
-            var totalPrice = addedItem.totalPrice;
-            if(!addedItem.discountApplied)
-            {
-                if($scope.customerName === "UNILEVER"){
-                    if(addedItem.id === 'classic'){
-                        if(addedItem.numberOfItems >= 2){
-                            addedItem.numberOfItems = parseInt(addedItem.numberOfItems)+1;
-                            totalPrice = parseInt(addedItem.numberOfItems) * parseInt(addedItem.price);
-                            addedItem.discountApplied = true;
-                        }
-                    }
-                }
-                else if($scope.customerName === 'APPLE'){
+    function addToCheckoutList() {
+        var addedItem = {};
+        customerSelected = $scope.customerName;
+        addedItem.name = $scope.selectedProduct.name;
+        addedItem.id = $scope.selectedProduct.id;
+        addedItem.price = $scope.selectedProduct.price;
+        addedItem.numberOfItems = $scope.numberOfItems;
+        addedItem.discountApplied = false;
+        addedItem.totalPrice = discountLogic(addedItem);
+        $scope.totalAddedItems.push(addedItem);
+        calculateTotalCheckoutValue();
+    }
 
-                }
-                else if($scope.customerName === 'NIKE'){
+    function calculateTotalCheckoutValue() {
+        $scope.totalCheckoutValue = 0;
+        $scope.totalAddedItems.forEach(function (addedItem, index) {
+            $scope.totalCheckoutValue = $scope.totalCheckoutValue + addedItem.totalPrice;
+        });
+    }
 
-                }
-                else if($scope.customerName === 'FORD'){
-
-                }
-                else{
-
-                }
-
-                totalPrice = totalPrice === addedItem.totalPrice ? parseInt(addedItem.numberOfItems) * parseInt(addedItem.price) : totalPrice ;
-                return totalPrice ;
-            }
-            else{
-                if($scope.customerName === "UNILEVER"){
-                    if(addedItem.id === 'classic'){
-                        totalPrice = parseInt(addedItem.numberOfItems) * parseInt(addedItem.price);
+    function discountLogic(addedItem) {
+        var totalPrice = addedItem.totalPrice;
+        if (!addedItem.discountApplied) {
+            if ($scope.customerName === "UNILEVER") {
+                if (addedItem.id === 'classic') {
+                    if (addedItem.numberOfItems >= 2) {
+                        addedItem.numberOfItems = parseFloat(addedItem.numberOfItems);
+                        totalPrice = (parseFloat($scope.numberOfItems) -1) * parseFloat(addedItem.price);
                         addedItem.discountApplied = true;
                     }
                 }
-
-                totalPrice = totalPrice === addedItem.totalPrice ? parseInt(addedItem.numberOfItems) * parseInt(addedItem.price) : totalPrice ;
-                return totalPrice ;
             }
+            else if ($scope.customerName === 'APPLE') {
+
+            }
+            else if ($scope.customerName === 'NIKE') {
+
+            }
+            else if ($scope.customerName === 'FORD') {
+
+            }
+            else {
+
+            }
+
+            totalPrice = totalPrice === addedItem.totalPrice ? parseFloat($scope.numberOfItems) * parseFloat(addedItem.price) : totalPrice;
+            return totalPrice;
         }
     }
 });
 
-app.controller("productCtrl", function($scope, $http) {
+app.controller("productCtrl", function ($scope, $http) {
     $scope.disableField = false;
     $scope.clearFields = clearFields;
-    $scope.addProduct = addProduct ;
+    $scope.addProduct = addProduct;
     $scope.editProduct = editProduct;
-    $scope.getAllProducts = getAllProducts ;
+    $scope.getAllProducts = getAllProducts;
 
     getAllProducts();
 
-    function getAllProducts(){
+    function getAllProducts() {
         $http.get('/products').
-        success(function(data) {
-            $scope.products = data
-        }).error(function(data) {
-            console.error("error pccured");
-        })
+            success(function (data) {
+                $scope.products = data
+            }).error(function (data) {
+                console.error("error pccured");
+            })
     }
 
-    function addProduct(){
-        var product = {} ;
-        product.id = $scope.id ;
+    function addProduct() {
+        var product = {};
+        product.id = $scope.id;
         product.name = $scope.name;
         product.price = $scope.price;
 
-        if(product.id == undefined || product.id == ""){
+        if (product.id == undefined || product.id == "") {
             alert("Product id is mandatory!");
             return false;
         }
 
         $http.post('/product/add', product).
-        success(function(data) {
-            console.log("product added successfully");
-            getAllProducts();
-        }).error(function(data) {
-            alert("Product id is mandatory!");
-            console.error("error in adding product");
-        })
+            success(function (data) {
+                console.log("product added successfully");
+                getAllProducts();
+            }).error(function (data) {
+                alert("Product id is mandatory!");
+                console.error("error in adding product");
+            })
     }
 
-    function editProduct(product){
-        $scope.id = product.id ;
+    function editProduct(product) {
+        $scope.id = product.id;
         $scope.name = product.name;
         $scope.price = product.price;
         $scope.disableField = true;
     }
 
-    function clearFields(){
+    function clearFields() {
         $scope.disableField = false;
-        $scope.id = "" ;
+        $scope.id = "";
         $scope.name = "";
         $scope.price = "";
     }
