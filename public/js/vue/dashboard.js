cb78fc93ce2601ea2acd6e64aa1c3716ec573d8a
mal90 <madkonline@gmail.com>
2017-06-10 03:08:57 +0530

    First commit vuejs

---
+++ public/js/vue/dashboard.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#events', //target element is div with id=element
+
+    data: {
+      event: { name: 'This is dashboard' }, //"data" is similer to scope in AngularJs . It is the glue between ctrl and view
+    },
+
+});
