6fd7fc4f90ac74ebf5b127a514ef8add6c02d38d
mal90 <SalimM@synergenhealth.com>
2017-10-06 08:19:23 -0500

    Vue intial migration commit

---
+++ migrating/adApp-vue/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container topmargin" id="dashboard">
+        <div class="col-md-12">
+            <div class="panel panel-info">
+                <div class="panel-heading">Dashboard</div>
+                <div class="panel-body">
+                    <div class="col-md-4">
+                    </div>
+                    <div class="col-md-4">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary">Manage Products</button>
+                            <button type="button" class="btn btn-success">Checkout</button>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+</style>
