263d84a12a2742c8605588d934fbbe5ead08f17c
mal90 <SalimM@synergenhealth.com>
2017-10-16 04:44:51 -0500

    Changes related to prducts , dashboard vue files

--- migrating/adApp-vue/src/main.js
+++ migrating/adApp-vue/src/main.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+
+import Products from './components/Products.vue'
+
+const routes = [
+	//route for the home route of the webpage
+  { path: '/', component: Products }
+]
+
+const router = new VueRouter({
+  routes, // short for routes: routes
+  mode: 'history'
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  //define the selector for the root component
+    el: '#app',
+    //pass the template to the root component
+    template: '<App/>',
+    //declare components that the root component can access
+    components: { App },
+    //pass in the router to the Vue instance
+    router
+  }).$mount('#app')//mount the router on the app
