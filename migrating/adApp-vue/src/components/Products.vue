135d4347dfd1863bcfd66542e0127ece8dc52e5e
mal90 <SalimM@synergenhealth.com>
2017-10-16 04:16:49 -0500

    Add products , dashboard vue files

---
+++ migrating/adApp-vue/src/components/Products.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container" id="Products">
+        <h1>This is products page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Products'
+}
+</script>
+
+<style>
+</style>
